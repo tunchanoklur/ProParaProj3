c4cadf80a1862a9b2c739417d92a0709ace736af
DESKTOP-IAR2C23\Tunchanok <kikpanda@gmail.com>
2017-11-26 13:37:14 +0700

    Level(kik ver)

--- src/proparaproj3/level.java
+++ src/proparaproj3/level.java
@@ -1,91 +1,74 @@
 package proparaproj3;
 
 import java.awt.*;
-import java.awt.event.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.*;
-import java.util.Random;
 
-public class Level extends JFrame{
-    private JLabel          contentpane;
-	//private Mycharacter     show_char;
-	private int frameWidth = 2000, frameHeight = 1000;
-        public static void main(String[] args) {
-            new Level();
-        }
-////
-	public Level()
-	{
-		setTitle("Chose level");
-		setBounds(200, 100, frameWidth, frameHeight);
-		setVisible(true);
-		setDefaultCloseOperation( WindowConstants.EXIT_ON_CLOSE );
-                
-		// set background image by using JLabel as contentpane
-		// Set layout to null because we'll move the label manually
-		setContentPane(contentpane = new JLabel());
-		MyImageIcon background = new MyImageIcon("level_wall.png");
-                contentpane.setIcon( background.resize(contentpane.getWidth(),
-			                                   contentpane.getHeight()) );
-             
-           
-       GridLayout layout = new GridLayout();
-       getContentPane().setLayout(layout);
-       // 3x2
-       layout.setColumns(3);
-       layout.setRows(1);
-       layout.setHgap(50);
-       layout.setVgap(0);
-       /////////////////////////////
-       JButton button1 = new JButton(new MyImageIcon("easy_but.png"));
-       JButton button2 = new JButton( new MyImageIcon("med_but.png"));
-       JButton button3 = new JButton( new MyImageIcon("ins_but.png"));
- 
-       button1.setPressedIcon(new MyImageIcon("easy_but.png"));
-       button2.setPressedIcon(new MyImageIcon("med_but.png"));
-       button3.setPressedIcon(new MyImageIcon("ins_but.png"));
+public class Level extends JFrame {
 
-       button1.setDisabledIcon(new MyImageIcon("go_but.png"));
-       button1.addActionListener(new ActionListener()
-               {
-                   public void actionPerformed (ActionEvent event)
-                   {
-                       JButton button = (JButton) event.getSource();
-                       button.setEnabled (false);
-                   }
-               }
-       );
-       ////////////////////////
-       button2.setDisabledIcon(new MyImageIcon("go_but.png"));
-       button2.addActionListener(new ActionListener()
-               {
-                   public void actionPerformed (ActionEvent event)
-                   {
-                       JButton button = (JButton) event.getSource();
-                       button.setEnabled (false);
-                   }
-               }
-       );
-       /////////////
-       button3.setDisabledIcon(new MyImageIcon("go_but.png"));
-       button3.addActionListener(new ActionListener()
-               {
-                   public void actionPerformed (ActionEvent event)
-                   {
-                       JButton button = (JButton) event.getSource();
-                       button.setEnabled (false);
-                   }
-               }
-       );
-            getContentPane().add(button1);
-            getContentPane().add(button2);
-            getContentPane().add(button3);
-            pack();
-                setLayout(null);
-                setSize(2000,1000);
-                setLocationRelativeTo(null);
-                setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                setVisible(true);
-        }
+    private JPanel contentpane;
+    private JLabel drawpane;
+    private MyImageIcon backgroundImg,goImg;
+    private JButton button[] = new JButton[3];
+    private int frameWidth = 2000, frameHeight = 1000;
+    
+    public static void main(String[] args) {
+        new Level();
+    }
+    
+    public Level() {
+        setTitle("Choose level");
+        setBounds(0,0, frameWidth, frameHeight);
+        setResizable(false);
+        setVisible(true);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        contentpane = (JPanel)getContentPane();
+        contentpane.setLayout(new BorderLayout());
+        backgroundImg = new MyImageIcon("picture/wallpaper/level_wall.png").resize(frameWidth,frameHeight);
+
+        drawpane = new JLabel();
+        drawpane.setIcon(backgroundImg);
+        drawpane.setLayout(null);
         
-}
+        /////////////////////////////
+        button[0] = new JButton(new MyImageIcon("picture/button/easy_but.png"));
+        button[1] = new JButton(new MyImageIcon("picture/button/med_but.png"));
+        button[2]= new JButton(new MyImageIcon("picture/button/ins_but.png"));
+        goImg = new MyImageIcon("picture/button/go_but.png");
+        
+        button[0].setDisabledIcon(goImg);
+        button[0].addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent event) {
+                JButton button = (JButton) event.getSource();
+                button.setEnabled(false);
+            }
+        }
+        );
+        ////////////////////////
+        button[1].setDisabledIcon(goImg);
+        button[1].addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent event) {
+                JButton button = (JButton) event.getSource();
+                button.setEnabled(false);
+            }
+        });
+        /////////////
+        button[2].setDisabledIcon(goImg);
+        button[2].addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent event) {
+                JButton button = (JButton) event.getSource();
+                button.setEnabled(false);
+            }
+        });
+        button[0].setBounds(225,800,200,100);
+        contentpane.add(button[0]);
+        button[1].setBounds(925,800,200,100);
+        contentpane.add(button[1]);
+        button[2].setBounds(1590,800,200,100);
+        contentpane.add(button[2]);
+        contentpane.add(drawpane, BorderLayout.CENTER);
+        validate();
+    }
 
+}
